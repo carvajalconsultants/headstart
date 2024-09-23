18344feafe55171da253ebcb85ae3b6fc94a391f
ZiaCodes <ziarzai@hotmail.com>
2024-09-23 10:43:47 +0500

    feat: about route lazy loaded

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/about')({
+export const Route = createLazyFileRoute('/about')({
   component: AboutComponent,
 })
 
