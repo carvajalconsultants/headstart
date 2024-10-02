8aed1ca845a3db0955ed59d25475989290bc6f04
ZiaCodes <ziarzai@hotmail.com>
2024-10-02 08:26:44 +0500

    feat: grafast fix for query type

--- test/app/grafastExchange.ts
+++ test/app/grafastExchange.ts
@@ -1,34 +1,30 @@
-import { hookArgs, execute } from 'grafast'
-import { pgl } from '../graphQLRouteHandler'
-import { CombinedError, Exchange, Operation, OperationResult } from 'urql'
-import { fromPromise, mergeMap, pipe } from 'wonka'
-import { schema } from '../graphQLRouteHandler'
 import type { Maybe } from 'grafast'
+import { execute, hookArgs } from 'grafast'
+import { CombinedError, Exchange, Operation, OperationResult } from 'urql'
+import { filter, fromPromise, mergeMap, pipe } from 'wonka'
+import { pgl } from '../graphQLRouteHandler'
 
 export const grafastExchange: Exchange = () => ops$ => {
   return pipe(
     ops$,
+    filter(operation => operation.kind === 'query'),
     mergeMap(operation => fromPromise(runGrafastQuery(operation)))
   )
 }
 
 async function runGrafastQuery(operation: Operation): Promise<OperationResult> {
   try {
-    const { kind, variables: variableValues, query: document } = operation;
-    if (kind !== "query") {
-      /* Only do queries (not subscriptions) on server side */
-      // TODO Find a way to skip this request
-    }
+    const { variables: variableValues, query: document } = operation
 
-    const schema = await pgl.getSchema();
+    const schema = await pgl.getSchema()
 
     const args = {
       resolvedPreset: pgl.getResolvedPreset(),
       schema,
       document,
-      variableValues: variableValues as Maybe<{ readonly [variable: string]: unknown; }>,
-    };
-    await hookArgs(args);
+      variableValues: variableValues as Maybe<{ readonly [variable: string]: unknown }>,
+    }
+    await hookArgs(args)
 
     const result = await execute(args)
 
