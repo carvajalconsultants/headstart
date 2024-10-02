0b1e1163d415f3aa144c6404c1ff466b90b44931
Miguel Carvajal <omar@carvajalonline.com>
2024-10-01 23:17:23 -0400

    fix: grafast exchange

--- test/app/grafastExchange.ts
+++ test/app/grafastExchange.ts
@@ -1,8 +1,9 @@
-import { execute } from 'grafast'
+import { hookArgs, execute } from 'grafast'
 import { pgl } from '../graphQLRouteHandler'
 import { CombinedError, Exchange, Operation, OperationResult } from 'urql'
 import { fromPromise, mergeMap, pipe } from 'wonka'
 import { schema } from '../graphQLRouteHandler'
+import type { Maybe } from 'grafast'
 
 export const grafastExchange: Exchange = () => ops$ => {
   return pipe(
@@ -13,14 +14,23 @@ export const grafastExchange: Exchange = () => ops$ => {
 
 async function runGrafastQuery(operation: Operation): Promise<OperationResult> {
   try {
-    const schemaInstance = await pgl.getSchema();
-    console.log(schemaInstance);
+    const { kind, variables: variableValues, query: document } = operation;
+    if (kind !== "query") {
+      /* Only do queries (not subscriptions) on server side */
+      // TODO Find a way to skip this request
+    }
 
-    const result = await execute({
-      schema: schemaInstance,
-      document: operation.query,
-      variableValues: operation.variables ?? {},
-    })
+    const schema = await pgl.getSchema();
+
+    const args = {
+      resolvedPreset: pgl.getResolvedPreset(),
+      schema,
+      document,
+      variableValues: variableValues as Maybe<{ readonly [variable: string]: unknown; }>,
+    };
+    await hookArgs(args);
+
+    const result = await execute(args)
 
     if (!result || typeof result !== 'object' || !('data' in result)) {
       throw new Error('Unexpected result format from execute')
