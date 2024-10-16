e98f85c78afa9be7c7c3ad285db703f5ade16b38
Miguel Carvajal <omar@carvajalonline.com>
2024-10-15 23:56:51 -0400

    refactor: removed old tests

--- test/app/api.ts
+++
@@ -1,5 +0,0 @@
-// app/api.ts
-import { createGraphQLRouteHandler } from '../graphQLRouteHandler'
-import preset from '../graphile.config'
-
-export default createGraphQLRouteHandler(preset)
--- test/app/client.tsx
+++
@@ -1,9 +0,0 @@
-// app/client.tsx
-/// <reference types="vinxi/types/client" />
-import { hydrateRoot } from 'react-dom/client'
-import { StartClient } from '@tanstack/start'
-import { createRouter } from './clientRouter'
-
-const router = createRouter()
-
-hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
