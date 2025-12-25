f0e85d284868d2f4adf081455439074982eb8836
Miguel Carvajal <omar@carvajalonline.com>
2025-12-25 08:20:08 -0500

    feat: grafast exchange now supports mutations and requestContext

--- grafastExchange.ts
+++ grafastExchange.ts
@@ -6,9 +6,18 @@ import {
 	type OperationResult,
 } from "urql";
 import { filter, fromPromise, mergeMap, pipe } from "wonka";
+import type { App, H3Event } from "h3";
+import {
+  getQuery,
+  getRequestHeaders,
+  getRequestProtocol,
+  readRawBody,
+} from "h3";
 
 import type { Maybe } from "grafast";
 import type { PostGraphileInstance } from "postgraphile";
+import { getEvent, getWebRequest } from "vinxi/http";
+import { GrafservBodyBuffer, normalizeRequest, processHeaders, RequestDigest } from "postgraphile/grafserv";
 
 export const grafastExchange = (pgl: PostGraphileInstance): Exchange => {
 	return () => (ops$) => {
@@ -16,12 +25,48 @@ export const grafastExchange = (pgl: PostGraphileInstance): Exchange => {
 			ops$,
 
 			// Skip anything that's not a query since that's all we can run
-			filter((operation) => operation.kind === "query"),
+			filter((operation) => operation.kind === "query" || operation.kind === "mutation"),
 
 			mergeMap((operation) => fromPromise(runGrafastQuery(pgl, operation))),
 		);
 	};
 };
+
+/* This is a direct copy from: https://github.com/graphile/crystal/blob/main/grafast/grafserv/src/servers/h3/v1/index.ts#L24 */
+function getDigest(event: H3Event): RequestDigest {
+  const req = event.node.req;
+  const res = event.node.res;
+  return {
+    httpVersionMajor: req.httpVersionMajor,
+    httpVersionMinor: req.httpVersionMinor,
+    isSecure: getRequestProtocol(event) === "https",
+    method: event.method,
+    path: event.path,
+    headers: processHeaders(getRequestHeaders(event)),
+    getQueryParams() {
+      return getQuery(event) as Record<string, string | string[]>;
+    },
+    async getBody() {
+      const buffer = await readRawBody(event, false);
+      if (!buffer) {
+        throw new Error("Failed to retrieve body from h3");
+      }
+      return {
+        type: "buffer",
+        buffer,
+      } as GrafservBodyBuffer;
+    },
+    requestContext: {
+      h3v1: {
+        event,
+      },
+      node: {
+        req,
+        res,
+      },
+    },
+  };
+}
 
 /**
  * Run the URQL query with Grafast, without making an HTTP request to ourselves (which is unnecessary overhead).
@@ -37,16 +82,25 @@ const runGrafastQuery = async (
 	try {
 		const { variables: variableValues, query: document } = operation;
 
-		const args = {
-			resolvedPreset: pgl.getResolvedPreset(),
-			schema: await pgl.getSchema(),
+		const { schema, resolvedPreset } = await pgl.getSchemaResult();
+
+		// Request/H3 info so we can run with it
+		const event = await getEvent();
+		const http = normalizeRequest(getDigest(event));
+
+		// Add the Graphile context to our args so Grafast can run
+		const args = await hookArgs({
+			schema,
 			document,
 			variableValues: variableValues as Maybe<{
 				readonly [variable: string]: unknown;
 			}>,
-		};
-		// Add the Graphile context to our args so Grafast can run
-		await hookArgs(args);
+			resolvedPreset,
+			requestContext: {
+				...http.requestContext,
+				http,
+			}
+		});
 
 		// Run the query with Grafast, to get the result from PostgreSQL
 		const result = await execute(args);
